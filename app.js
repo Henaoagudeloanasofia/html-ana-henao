82ca09fd936741784d4904a993d64603c3e3271e
Henaoagudeloanasofia <123569782+Henaoagudeloanasofia@users.noreply.github.com>
2023-02-06 12:04:54 -0500

    cambios(06/02/23)

---
+++ app.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const cardAdj = [
+    {
+      name: "Torre Eiffel",
+      img: "imagenes/torre eiffel.jpg"
+    },
+    {
+      name: "burj al arab",
+      img: "imagenes/burj al arab.jpg"
+    }
+  ];
+});
