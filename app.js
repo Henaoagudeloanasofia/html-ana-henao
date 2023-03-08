11e2f0af93659d7531f24854e96e01e02fc64b50
Henaoagudeloanasofia <123569782+Henaoagudeloanasofia@users.noreply.github.com>
2023-03-08 09:24:50 -0500

    cambios(08/03/23)

--- app.js
+++ app.js
@@ -52,18 +52,31 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const cuadricula = document.querySelector(".cuadricula");
   const resultado = document.querySelector("#resultado");
-  var cartasEscogidasId = [];
+  var cartasEscogidas = [];
   var cartasEscogidasId = [];
   var cartasGanadas = [];
   //----------------- lectura03 ---------------------------//
   function crearTablero() {
-    for (let i = 0; i < cardAdj.length; i++)
+    for (let i = 0; i < cardAdj.length; i++) {
       var carta = document.createElement("img");
-    carta.setAttribute("src", "imagenes/reverso.png");
+      carta.setAttribute("src", "imagenes/reverso.png");
 
-    carta.setAttribute("data-id", i);
-    carta.addEventListener("click", voltearCarta);
+      carta.setAttribute("data-id", i);
+      carta.addEventListener("click", voltearCarta);
 
-    cuadricula.appendChild(carta);
+      cuadricula.appendChild(carta);
+    }
   }
+
+  //-------------------------lectura04-------------------------//
+  function voltearCarta() {
+    var cardId = this.getAttribute("data-id");
+    cartasEscogidas.push(cardAdj[cardId].name);
+    cartasEscogidasId.push(cardId);
+    this.setAttribute("src", cardAdj[cardId].img);
+    if (cartasEscogidas.length === 2) {
+      setTimeout(verificarPareja, 1000);
+    }
+  }
+  crearTablero();
 });
